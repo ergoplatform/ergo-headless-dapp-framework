1da421cf2a6e9bff88dfde9c7de2bc3f8dcb81d0
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2020-12-31 13:52:27 +0100

    Code cleanup

--- src/tx_creation.rs
+++ src/tx_creation.rs
@@ -1,19 +1,16 @@
-// This file holds a number of functions which aid in tx creation.
+// This file holds a number of functions which aid in tx creation and using the Tx Assembler.
 use crate::error::{HeadlessDappError, Result};
 use crate::{
-    encoding::address_string_to_ergo_tree, encoding::serialize_p2s_from_ergo_tree,
-    P2PKAddressString, P2SAddressString,
+    encoding::address_string_to_ergo_tree,
 };
 use crate::{BlockHeight, ErgoAddressString, NanoErg};
 use ergo_lib::chain::ergo_box::{BoxValue, ErgoBox, ErgoBoxCandidate, NonMandatoryRegisters};
 use ergo_lib::chain::token::{Token, TokenAmount};
-use ergo_lib::{ast::constant::Constant, chain::transaction::unsigned::UnsignedTransaction};
-use ergo_lib_wasm::transaction::UnsignedTransaction as WUnsignedTransaction;
-use json::object;
+use ergo_lib::{ast::constant::Constant,};
 use std::convert::TryFrom;
-use wasm_bindgen::prelude::*;
 
-/// Create an `ErgoBoxCandidate`
+
+/// Helper function for creating an `ErgoBoxCandidate`
 pub fn create_candidate(
     value: NanoErg,
     output_address: &ErgoAddressString,
@@ -82,86 +79,4 @@ pub fn find_and_sum_other_tokens(
         }
     }
     new_tokens
-}
-
-/// Given an `UnsignedTransaction`, builds a JSON `String` which
-/// is formatted as a transaction spec for working with the
-/// Transaction Assembler Service.
-#[wasm_bindgen]
-pub fn w_unsigned_transaction_to_assembler_spec(
-    wrapped_unsigned_tx: WUnsignedTransaction,
-    transaction_fee: NanoErg,
-) -> String {
-    let unsigned_tx = wrapped_unsigned_tx.into();
-    unsigned_transaction_to_assembler_spec(unsigned_tx, transaction_fee)
-}
-
-/// Given an `UnsignedTransaction`, builds a JSON `String` which
-/// is formatted as a transaction spec for working with the
-/// Transaction Assembler Service.
-pub fn unsigned_transaction_to_assembler_spec(
-    unsigned_tx: UnsignedTransaction,
-    transaction_fee: NanoErg,
-) -> String {
-    let mut tx_spec = object! {
-        "requests":  [],
-    };
-
-    for i in 0..unsigned_tx.output_candidates.len() {
-        let output = unsigned_tx.output_candidates[i].clone();
-        // Base values
-        tx_spec["requests"][i]["value"] = output.value.as_u64().clone().into();
-        tx_spec["requests"][i]["address"] = serialize_p2s_from_ergo_tree(output.ergo_tree).into();
-
-        // Tokens
-        for n in 0..output.tokens.len() {
-            let token = output.tokens[n].clone();
-            let tok_id: String = token.token_id.0.into();
-            let tok_amount: u64 = token.amount.into();
-            tx_spec["requests"][i]["assets"][n]["tokenId"] = tok_id.into();
-            tx_spec["requests"][i]["assets"][n]["amount"] = tok_amount.into();
-        }
-
-        // Registers
-        let registers = output.additional_registers.get_ordered_values();
-        for y in 0..registers.len() {
-            if y == 0 {
-                tx_spec["requests"][i]["registers"]["R4"] = registers[y].base16_str().into();
-            }
-            if y == 1 {
-                tx_spec["requests"][i]["registers"]["R5"] = registers[y].base16_str().into();
-            }
-            if y == 2 {
-                tx_spec["requests"][i]["registers"]["R6"] = registers[y].base16_str().into();
-            }
-            if y == 3 {
-                tx_spec["requests"][i]["registers"]["R7"] = registers[y].base16_str().into();
-            }
-            if y == 4 {
-                tx_spec["requests"][i]["registers"]["R8"] = registers[y].base16_str().into();
-            }
-            if y == 5 {
-                tx_spec["requests"][i]["registers"]["R9"] = registers[y].base16_str().into();
-            }
-        }
-
-        // Fee
-        tx_spec["fee"] = transaction_fee.into();
-
-        // Inputs
-        let inputs = unsigned_tx.inputs.clone();
-        for y in 0..inputs.len() {
-            let box_id: String = inputs[y].box_id.clone().into();
-            tx_spec["inputs"][y] = box_id.into();
-        }
-
-        // Inputs
-        let data_inputs = unsigned_tx.data_inputs.clone();
-        for y in 0..data_inputs.len() {
-            let box_id: String = data_inputs[y].box_id.clone().into();
-            tx_spec["dataInputs"][y] = box_id.into();
-        }
-    }
-
-    tx_spec.pretty(2)
 }
